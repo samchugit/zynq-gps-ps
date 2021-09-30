4d35713eb3ea501c9ecdf2e9187e7a192452ecfe
Sam Chu <samchu-social@outlook.com>
2021-09-30 10:36:10 +0800

    fix: change BRAM offset to data offset

--- scripts/gps-acq.sh
+++ scripts/gps-acq.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./build/cli/axi-bram write -b50000000 -d10000000 -o40 -l1 1
+./build/cli/axi-bram write -b50000000 -d10000000 -o10 -l1 1
 sleep 0.001
-./build/cli/axi-bram write -b50000000 -d10000000 -o4004 -l1 1
+./build/cli/axi-bram write -b50000000 -d10000000 -o1001 -l1 1
 sleep 0.001
-./build/cli/axi-bram write -b50000000 -d10000000 -o4000 -l1 1
+./build/cli/axi-bram write -b50000000 -d10000000 -o1000 -l1 1
 sleep 0.001
-./build/cli/axi-bram write -b50000000 -d10000000 -o4000 -l1 0
+./build/cli/axi-bram write -b50000000 -d10000000 -o1000 -l1 0
