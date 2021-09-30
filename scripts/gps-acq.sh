66fe1348742ef51f5649b03765d2a68dea0863cc
Sam Chu <samchu-social@outlook.com>
2021-09-30 16:08:54 +0800

    fix(gps-acq.sh): change mode to '3'

--- scripts/gps-acq.sh
+++ scripts/gps-acq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./build/cli/axi-bram write -b50000000 -d10000000 -o10 -l1 1
+./build/cli/axi-bram write -b50000000 -d10000000 -o10 -l1 3
 sleep 0.001
 ./build/cli/axi-bram write -b50000000 -d10000000 -o1001 -l1 1
 sleep 0.001
